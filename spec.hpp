4cefa257d43280828eeab1125f8473da4817ab0a
degarashi <slice013@gmail.com>
2016-11-02 10:50:27 +0900

    enumをspineのDefineEnumマクロに置換

--- spec.hpp
+++ spec.hpp
@@ -1,28 +1,31 @@
 #pragma once
 #include "spine/singleton.hpp"
+#include "spine/enum.hpp"
 
 namespace rev {
 	//! 実行環境に関する情報を取得
 	class Spec : public spi::Singleton<Spec> {
 		public:
-			enum Feature : uint32_t {
-				F_3DNow = 0x01,
-				F_AltiVec = 0x02,
-				F_MMX = 0x04,
-				F_RDTSC = 0x08,
-				F_SSE = 0x10,
-				F_SSE2 = 0x11,
-				F_SSE3 = 0x12,
-				F_SSE41 = 0x14,
-				F_SSE42 = 0x18
-			};
-			enum class PStatN {
-				Unknown,
-				OnBattery,
-				NoBattery,
-				Charging,
-				Charged
-			};
+			DefineEnumPair(
+				Feature,
+				((F_3DNow)(0x01))
+				((F_AltiVec)(0x02))
+				((F_MMX)(0x04))
+				((F_RDTSC)(0x08))
+				((F_SSE)(0x10))
+				((F_SSE2)(0x11))
+				((F_SSE3)(0x12))
+				((F_SSE41)(0x14))
+				((F_SSE42)(0x18))
+			);
+			DefineEnum(
+				PStatN,
+				(Unknown)
+				(OnBattery)
+				(NoBattery)
+				(Charging)
+				(Charged)
+			);
 			struct PStat {
 				PStatN	state;
 				int		seconds,
