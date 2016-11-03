8348275b0a3003c4f3f9f2f8469b0d0b86c55315
degarashi <slice013@gmail.com>
2016-11-03 21:00:46 +0900

    camera2dを移植

---
+++ camera2d.cpp
@@ -0,0 +1,51 @@
+#include "camera2d.hpp"
+
+namespace rev {
+	Camera2D::Camera2D() {
+		setAccum(std::rand());
+		setAspectRatio(1.f);
+	}
+	bool Camera2D::_refresh(Accum::value_t& acc, Accum*) const {
+		const auto ret = _rflag.getWithCheck(this, acc);
+		const bool b = ret.flag;
+		if(b) {
+			// カウンタをインクリメント
+			++acc;
+		}
+		return b;
+	}
+	bool Camera2D::_refresh(View::value_t& m, View*) const {
+		const auto ret = _rflag.getWithCheck(this, m);
+		auto& ps = *std::get<0>(ret);
+		const bool b = ret.flag;
+		if(b)
+			m = ps.getToLocal();
+		return b;
+	}
+	bool Camera2D::_refresh(ViewInv::value_t& m, ViewInv*) const {
+		m = getView().inversion();
+		return true;
+	}
+	bool Camera2D::_refresh(Proj::value_t& m, Proj*) const {
+		m = AMat3::Scaling({1.f / getAspectRatio(), 1, 1});
+		return true;
+	}
+	bool Camera2D::_refresh(ViewProj::value_t& m, ViewProj*) const {
+		m = getView() * getProj();
+		return true;
+	}
+	bool Camera2D::_refresh(ViewProjInv::value_t& m, ViewProjInv*) const {
+		m = getViewProj().inversion();
+		return true;
+	}
+
+	Camera2D::Vec2 Camera2D::vp2w(const Vec2& pos) const {
+		return (pos.convert<3>() * getViewProjInv()).convert<2>();
+	}
+	Camera2D::Vec2 Camera2D::v2w(const Vec2& pos) const {
+		return (pos.convertI<3,2>(1) * getViewInv()).convert<2>();
+	}
+	const char* Camera2D::getResourceName() const noexcept {
+		return "Camera2D";
+	}
+}
---
+++ camera2d.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include "spine/rflag.hpp"
+#include "pose2d.hpp"
+#include "frea/matrix.hpp"
+#include "handle.hpp"
+#include "spine/resmgr.hpp"
+
+namespace rev {
+	//! 2Dカメラ姿勢クラス
+	/*!
+		2Dゲームの他にミニマップ表示などで使用
+		姿勢の保持はPose2Dクラスが行い，カメラ固有の変数だけを持つ
+	*/
+	class Camera2D : public lubee::CheckAlign<Camera2D>, public Resource {
+		private:
+			struct Pose;
+			struct Getter : spi::RFlag_Getter<uint32_t> {
+				using RFlag_Getter::operator ();
+				counter_t operator()(const Pose2D& pose, Pose*, const Camera2D&) const {
+					return pose.getAccum();
+				}
+			};
+			using AMat3 = frea::AMat3;
+			using Vec2 = frea::Vec2;
+			using View_t = spi::AcCheck<AMat3, Getter>;
+			using Accum_t = spi::AcCheck<lubee::Wrapper<uint32_t>, Getter>;
+			#define SEQ \
+				((Pose)(Pose2D)) \
+				((View)(View_t)(Pose)) \
+				((ViewInv)(AMat3)(View)) \
+				((AspectRatio)(float)) \
+				((Proj)(AMat3)(AspectRatio)) \
+				((ViewProj)(AMat3)(View)(Proj)) \
+				((ViewProjInv)(AMat3)(ViewProj)) \
+				((Accum)(Accum_t)(Pose)(AspectRatio))
+			RFLAG_DEFINE(Camera2D, SEQ)
+			RFLAG_SETMETHOD(Accum)
+		public:
+			RFLAG_GETMETHOD_DEFINE(SEQ)
+			RFLAG_SETMETHOD_DEFINE(SEQ)
+			RFLAG_REFMETHOD(Pose)
+			#undef SEQ
+
+			Camera2D();
+			//! Projection後の座標をワールド座標へ変換
+			Vec2 vp2w(const Vec2& pos) const;
+			//! View座標をワールド座標へ変換
+			Vec2 v2w(const Vec2& pos) const;
+			const char* getResourceName() const noexcept override;
+	};
+	#define mgr_cam2d (::rev::Camera2DMgr::ref())
+	class Camera2DMgr : public spi::ResMgr<Camera2D, lubee::AlignedPool<Camera2D>> {};
+}
