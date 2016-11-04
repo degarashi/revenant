debec6b0f9bafcb8f3cf8f6aa64242d3f22e077f
degarashi <slice013@gmail.com>
2016-11-04 09:42:59 +0900

    camera3dを移植 コメントアウトしてある部分は当たり判定に関わるので保留

---
+++ camera3d.cpp
@@ -0,0 +1,101 @@
+#include "camera3d.hpp"
+
+namespace rev {
+	bool Camera3D::_refresh(typename Accum::value_t& acc, Accum*) const {
+		const auto ret = _rflag.getWithCheck(this, acc);
+		const bool b = ret.flag;
+		if(b) {
+			// カウンタをインクリメント
+			++acc;
+		}
+		return b;
+	}
+	bool Camera3D::_refresh(typename View::value_t& m, View*) const {
+		auto ret = _rflag.getWithCheck(this, m);
+		auto& ps = *std::get<0>(ret);
+		const bool b = ret.flag;
+		if(b) {
+			// 回転を一端quatに変換
+			const Quat& q = ps.getRotation();
+			m = AMat4::Identity();
+			m.getRow<3>() = -ps.getOffset().convert<4>();
+
+			const Quat tq = q.inversion();
+			m *= tq.asMat33().convertI<4,4,3>(1);
+		}
+		return b;
+	}
+	bool Camera3D::_refresh(AMat4& m, Proj*) const {
+		m = AMat4::PerspectiveFov(getFov(), getAspect(), {getNearZ(), getFarZ()});
+		return true;
+	}
+	bool Camera3D::_refresh(AMat4& m, ViewProj*) const {
+		m = getView() * getProj();
+		return true;
+	}
+	bool Camera3D::_refresh(AMat4& m, ViewProjInv*) const {
+		m = getViewProj().inversion();
+		return true;
+	}
+	// bool Camera3D::_refresh(Frustum& vf, VFrustum*) const {
+	// 	// UP軸とZ軸を算出
+	// 	AMat44 mat;
+	// 	getView().convertA44().inversion(mat);
+	// 	mat.getRow(3) = AVec4(0,0,0,1);
+	// 	AVec4 zAxis(0,0,1,0),
+	// 		yAxis(0,1,0,0);
+	// 	zAxis *= mat;
+	// 	yAxis *= mat;
+	//
+	// 	auto& ps = getPose();
+	// 	vf = Frustum(ps.getOffset(), zAxis.asVec3(), yAxis.asVec3(), getFov(), getFarZ(), getAspect());
+	// 	return true;
+	// }
+
+	Camera3D::Camera3D() {
+		refPose().setAll(Vec3(0,0,-10), Quat(0,0,0,1), Vec3(1,1,1));
+		setAccum(1);
+
+		setNearZ(1.f);
+		setFarZ(1e5f);
+		setFov(frea::DegF(90.0f));
+		setAspect(1.4f / 1.f);
+	}
+	void Camera3D::setZPlane(const float n, const float f) {
+		setNearZ(n);
+		setFarZ(f);
+	}
+	Camera3D::Vec3 Camera3D::unproject(const Vec3& vsPos) const {
+		const AMat4& mI = getViewProjInv();
+		return (vsPos.convertI<4,3>(1) * mI).asVec3Coord();
+	}
+	Camera3D::Vec3x2 Camera3D::unprojectVec(const Vec2& vsPos) const {
+		const AMat4& mI = getViewProjInv();
+		return Vec3x2(
+			(AVec4(vsPos.x, vsPos.y, 0, 1) * mI).asVec3Coord(),
+			(AVec4(vsPos.x, vsPos.y, 1, 1) * mI).asVec3Coord()
+		);
+	}
+	Camera3D::Vec3 Camera3D::vp2wp(const Vec3& vp) const {
+		return (vp.convertI<4,3>(1) * getViewProjInv()).asVec3Coord();
+	}
+	Camera3D::Plane Camera3D::getNearPlane() const {
+		auto& ps = getPose();
+		const Vec3 dir = ps.getDir();
+		return Plane::FromPtDir(ps.getOffset() + dir*getNearZ(), dir);
+	}
+	// カメラの変換手順としては offset -> rotation だがPose3Dの変換は rotation -> offsetなので注意！
+	// Frustum Camera3D::getNearFrustum() const {
+	// 	Frustum fr;
+	// 	float t = std::tan(getFov().get()/2);
+	// 	fr.setScale({t*getAspect(), t, getNearZ()*8});
+	// 	auto& ps = getPose();
+	// 	fr.setRot(ps.getRot());
+	// 	fr.setOffset(ps.getOffset());
+	// 	return fr;
+	// }
+	const char* Camera3D::getResourceName() const noexcept {
+		return "Camera3D";
+	}
+}
+
---
+++ camera3d.hpp
@@ -0,0 +1,65 @@
+#pragma once
+#include "spine/rflag.hpp"
+#include "frea/matrix.hpp"
+#include "frea/plane.hpp"
+#include "handle.hpp"
+#include "pose3d.hpp"
+#include "spine/resmgr.hpp"
+
+namespace rev {
+	/*! 姿勢の保持はPose3Dクラスが行い，カメラ固有の変数だけを持つ */
+	class Camera3D : public lubee::CheckAlign<Camera3D>, public Resource {
+		private:
+			using AMat4 = frea::AMat4;
+			using AVec4 = frea::AVec4;
+			using Vec3 = frea::Vec3;
+			using Vec2 = frea::Vec2;
+			using RadF = frea::RadF;
+			using Quat = frea::Quat;
+			using Plane = frea::Plane;
+			struct Pose;
+			struct Getter : spi::RFlag_Getter<uint32_t> {
+				using RFlag_Getter::operator ();
+				counter_t operator()(const Pose3D& pose, Pose*, const Camera3D&) const {
+					return pose.getAccum();
+				}
+			};
+			using View_t = spi::AcCheck<AMat4, Getter>;
+			using Accum_t = spi::AcCheck<lubee::Wrapper<uint32_t>, Getter>;
+			#define SEQ \
+				((Pose)(Pose3D)) \
+				((View)(View_t)(Pose)) \
+				((Fov)(RadF)) \
+				((Aspect)(float)) \
+				((NearZ)(float)) \
+				((FarZ)(float)) \
+				((Proj)(AMat4)(Fov)(Aspect)(NearZ)(FarZ)) \
+				((ViewProj)(AMat4)(View)(Proj)) \
+				((ViewProjInv)(AMat4)(ViewProj)) \
+				((Accum)(Accum_t)(Pose)(Fov)(Aspect)(NearZ)(FarZ))
+				// ((VFrustum)(boom::geo3d::Frustum)(View)(Fov)(Aspect)(NearZ)(FarZ))
+			RFLAG_DEFINE(Camera3D, SEQ)
+			RFLAG_SETMETHOD(Accum)
+		public:
+			using Vec3x2 = std::pair<Vec3, Vec3>;
+			Camera3D();
+
+			RFLAG_GETMETHOD_DEFINE(SEQ)
+			RFLAG_SETMETHOD_DEFINE(SEQ)
+			RFLAG_REFMETHOD_DEFINE(SEQ)
+			#undef SEQ
+
+			void setZPlane(float n, float f);
+
+			Plane getNearPlane() const;
+			// boom::geo3d::Frustum getNearFrustum() const;
+			//! ビューポート座標をワールド座標系に変換
+			Vec3 unproject(const Vec3& vsPos) const;		// 単体
+			Vec3x2 unprojectVec(const Vec2& vsPos) const;	// (Near,Far)
+			//! ビューポート座標からワールド座標(FarPlane位置)を取得
+			Vec3 vp2wp(const Vec3& vp) const;
+			const char* getResourceName() const noexcept override;
+	};
+	#define mgr_cam (::rev::Camera3DMgr::ref())
+	class Camera3DMgr : public spi::ResMgr<Camera3D, lubee::AlignedPool<Camera3D>> {};
+}
