2fabf392f694dbd38660db9837a7e894f66943fe
degarashi <slice013@gmail.com>
2016-11-09 18:05:05 +0900

    URI_File: シリアライズ対応

---
+++ serialization/uri_file.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "serialization/urihandler.hpp"
+#include "../uri_file.hpp"
+
+namespace rev {
+	template <class Ar>
+	void serialize(Ar& ar, URI_File& uf) {
+		ar(uf._basePath);
+	}
+}
+namespace cereal {
+	template <>
+	struct LoadAndConstruct<::rev::URI_File> {
+		template <class Ar>
+		static void load_and_construct(Ar& ar, cereal::construct<::rev::URI_File>& construct) {
+			::rev::PathBlock pb;
+			ar(pb);
+			construct(pb.plain_utf8());
+		}
+	};
+}
+CEREAL_REGISTER_TYPE(::rev::URI_File);
+CEREAL_REGISTER_POLYMORPHIC_RELATION(::rev::URIHandler, ::rev::URI_File);
--- uri_file.hpp
+++ uri_file.hpp
@@ -2,12 +2,21 @@
 #include "urihandler.hpp"
 #include "path.hpp"
 
+namespace cereal {
+	template <class T>
+	struct LoadAndConstruct;
+}
 namespace rev {
 	//! ファイルシステムからのファイル読み込み
 	class URI_File : public URIHandler {
 		private:
 			PathBlock		_basePath;
 			HRW _openURI(const PathBlock& pb, int access);
+
+			template <class Ar>
+			friend void serialize(Ar&, URI_File&);
+			template <class T>
+			friend struct cereal::LoadAndConstruct;
 		public:
 			URI_File(To32Str path);
 			static bool Capable(const URI& uri, int access);
