e8014ce5a4d3efa0c50ffae7d4ca6e14019e4b11
degarashi <slice013@gmail.com>
2016-12-19 19:12:39 +0900

    LCV: テストコードの一部クラス宣言をヘッダへ移動

--- tests/lcv_test.cpp
+++ tests/lcv_test.cpp
@@ -1,76 +1,9 @@
-#include "luatest.hpp"
+#include "lcvtest.hpp"
 #include "../lvalue.hpp"
-#include "lubee/rect.hpp"
-#include "../sdl_rw.hpp"
 #include "../dir.hpp"
 
-namespace std {
-	template <class T>
-	bool operator == (const weak_ptr<T>& p0, const weak_ptr<T>& p1) noexcept {
-		return p0.lock() == p1.lock();
-	}
-}
 namespace rev {
 	namespace test {
-		template <class T>
-		bool PreciseCompare(const T& v0, const T& v1) {
-			return v0 == v1;
-		}
-		bool PreciseCompare(const LCValue& v0, const LCValue& v1) noexcept {
-			return v0.preciseCompare(v1);
-		}
-		bool PreciseCompare(const char* c0, const char* c1) {
-			return std::string(c0) == std::string(c1);
-		}
-		template <class T>
-		struct LCV_Test : LuaTest {
-			using value_t = T;
-			using LCV_t = LCV<T>;
-			LCV_t	_lcv;
-
-			void pushTest() {
-				auto& lsp = this->_lsp;
-				lua_State *const ls = lsp->getLS();
-				lsp->checkStack(32);
-				constexpr int Max_Other = 8;
-				LCValue prev[Max_Other],
-						post[Max_Other];
-				const int nprev = _rdi({0,Max_Other-1}),
-						npost = _rdi({0,Max_Other-1});
-				// 前に関係ない値を挟む
-				for(int i=0 ; i<nprev ; i++) {
-					prev[i] = genLCValue(c_luaTypes_key);
-					lsp->push(prev[i]);
-				}
-				// LCV経由でPush
-				const auto v0 = genValue<value_t>();
-				const int cur = _lcv(ls, v0);
-				ASSERT_EQ(nprev+cur, lsp->getTop());
-				// 後に関係ない値を加える
-				for(int i=0 ; i<npost ; i++) {
-					post[i] = genLCValue(c_luaTypes_key);
-					lsp->push(post[i]);
-				}
-				// LCV経由で値を取得(正のインデックス & 負のインデックス)
-				const auto v1 = _lcv(nprev+1, ls, nullptr),
-							v2 = _lcv(lsp->getTop()-npost, ls, nullptr);
-				ASSERT_TRUE(PreciseCompare(v0, decltype(v0)(v1)));
-				ASSERT_TRUE(PreciseCompare(v1, v2));
-
-				// スタックの他の値は変化なし
-				for(int i=0 ; i<nprev ; i++)
-					ASSERT_TRUE(prev[i].preciseCompare(lsp->toLCValue(i+1)));
-				for(int i=0 ; i<npost ; i++)
-					ASSERT_TRUE(post[i].preciseCompare(lsp->toLCValue(nprev+cur+1+i)));
-			}
-			void typeTest() {
-				// LCV()が返す型タイプが実際Pushした時と一致しているか確認
-				auto& lsp = this->_lsp;
-				const auto val = genValue<value_t>();
-				_lcv(lsp->getLS(), val);
-				ASSERT_EQ(_lcv(val), lsp->type(-1));
-			}
-		};
 		using Types0 = ::testing::Types<
 			LuaNil, bool, const char*, std::string,
 			lua_Integer, lua_Number, lua_OtherNumber,
@@ -87,22 +20,8 @@ namespace rev {
 			void_sp, void_wp,
 			std::shared_ptr<int>, std::weak_ptr<int>
 		>;
-
 		template <class T>
-		class LCV_Test1 : public LCV_Test<T> {
-			private:
-				static std::unique_ptr<RWMgr> s_rw;
-			public:
-				static void SetUpTestCase() {
-					s_rw = std::make_unique<RWMgr>("APP", "ORG");
-				}
-				static void TearDownTestCase() {
-					s_rw.reset();
-				}
-		};
-		template <class T>
-		std::unique_ptr<RWMgr> LCV_Test1<T>::s_rw;
-
+		using LCV_Test1 = LCV_TestRW<T>;
 		TYPED_TEST_CASE(LCV_Test1, Types1);
 		TYPED_TEST(LCV_Test1, Push) {
 			auto& lsp = this->_lsp;
---
+++ tests/lcvtest.hpp
@@ -0,0 +1,88 @@
+#pragma once
+#include "luatest.hpp"
+#include "../sdl_rw.hpp"
+
+namespace std {
+	template <class T>
+	bool operator == (const weak_ptr<T>& p0, const weak_ptr<T>& p1) noexcept {
+		return p0.lock() == p1.lock();
+	}
+}
+namespace rev {
+	namespace test {
+		template <class T>
+		bool PreciseCompare(const T& v0, const T& v1) {
+			return v0 == v1;
+		}
+		bool PreciseCompare(const LCValue& v0, const LCValue& v1) noexcept {
+			return v0.preciseCompare(v1);
+		}
+		bool PreciseCompare(const char* c0, const char* c1) {
+			return std::string(c0) == std::string(c1);
+		}
+		template <class T>
+		struct LCV_Test : LuaTest {
+			using value_t = T;
+			using LCV_t = LCV<T>;
+			LCV_t	_lcv;
+
+			void pushTest() {
+				auto& lsp = this->_lsp;
+				lua_State *const ls = lsp->getLS();
+				lsp->checkStack(32);
+				constexpr int Max_Other = 8;
+				LCValue prev[Max_Other],
+						post[Max_Other];
+				const int nprev = _rdi({0,Max_Other-1}),
+						npost = _rdi({0,Max_Other-1});
+				// 前に関係ない値を挟む
+				for(int i=0 ; i<nprev ; i++) {
+					prev[i] = genLCValue(c_luaTypes_key);
+					lsp->push(prev[i]);
+				}
+				// LCV経由でPush
+				const auto v0 = genValue<value_t>();
+				const int cur = _lcv(ls, v0);
+				ASSERT_EQ(nprev+cur, lsp->getTop());
+				// 後に関係ない値を加える
+				for(int i=0 ; i<npost ; i++) {
+					post[i] = genLCValue(c_luaTypes_key);
+					lsp->push(post[i]);
+				}
+				// LCV経由で値を取得(正のインデックス & 負のインデックス)
+				const auto v1 = _lcv(nprev+1, ls, nullptr),
+							v2 = _lcv(lsp->getTop()-npost, ls, nullptr);
+				ASSERT_TRUE(PreciseCompare(v0, decltype(v0)(v1)));
+				ASSERT_TRUE(PreciseCompare(v1, v2));
+
+				// スタックの他の値は変化なし
+				for(int i=0 ; i<nprev ; i++)
+					ASSERT_TRUE(prev[i].preciseCompare(lsp->toLCValue(i+1)));
+				for(int i=0 ; i<npost ; i++)
+					ASSERT_TRUE(post[i].preciseCompare(lsp->toLCValue(nprev+cur+1+i)));
+			}
+			void typeTest() {
+				// LCV()が返す型タイプが実際Pushした時と一致しているか確認
+				auto& lsp = this->_lsp;
+				const auto val = genValue<value_t>();
+				_lcv(lsp->getLS(), val);
+				ASSERT_EQ(_lcv(val), lsp->type(-1));
+			}
+		};
+
+		template <class T>
+		class LCV_TestRW : public LCV_Test<T> {
+			private:
+				static std::unique_ptr<RWMgr> s_rw;
+			public:
+				static void SetUpTestCase() {
+					s_rw = std::make_unique<RWMgr>("APP", "ORG");
+				}
+				static void TearDownTestCase() {
+					s_rw.reset();
+				}
+		};
+		template <class T>
+		std::unique_ptr<RWMgr> LCV_TestRW<T>::s_rw;
+	}
+}
