5c53c33fba6e871fe0be93c17d06bc5475900c2d
degarashi <slice013@gmail.com>
2016-12-31 15:39:12 +0900

    LCV: expquatテストコード

---
+++ tests/lcv_expquattest.cpp
@@ -0,0 +1,25 @@
+#include "lcv_classtest.hpp"
+
+namespace rev {
+	namespace test {
+		template <class T>
+		struct LCV_EQuat : LCV_ClassTest<T> {
+		};
+		using TypesE = ::testing::Types<
+			LCVPair1<frea::ExpQuat>, LCVPair2<frea::AExpQuat, frea::ExpQuat>
+		>;
+		TYPED_TEST_CASE(LCV_EQuat, TypesE);
+		TYPED_TEST(LCV_EQuat, Push) {
+			this->pushTest();
+		}
+		TYPED_TEST(LCV_EQuat, Type) {
+			this->typeTest();
+		}
+		TYPED_TEST(LCV_EQuat, ReadMember) {
+			this->readMemberTestXYZW();
+		}
+		TYPED_TEST(LCV_EQuat, WriteMember) {
+			this->writeMemberTestXYZW();
+		}
+	}
+}
