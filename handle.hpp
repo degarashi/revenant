be40ef8abc5c9d88e5844eb6658961ba68f0d2ec
degarashi <slice013@gmail.com>
2017-10-27 20:18:23 +0900

    Resource: シリアライズ時に比較を行うためのインタフェース

--- handle.hpp
+++ handle.hpp
@@ -1,11 +1,22 @@
 #pragma once
 #include <memory>
+#include "lubee/deep_cmp.hpp"
 
 namespace rev {
+	#define DEF_RESOURCE_EQUAL bool deepCmp(const Resource& r) const noexcept { return _deepCmp(*this, r); }
 	struct Resource {
 		virtual ~Resource() {}
 		virtual const char* getResourceName() const noexcept = 0;
+		template <class Self>
+		bool _deepCmp(const Self& self, const Resource& r) const noexcept {
+			const Self* p = dynamic_cast<const Self*>(&r);
+			if(p)
+				return lubee::DeepCmp()(self, *p);
+			return false;
+		}
+		DEF_RESOURCE_EQUAL
 	};
+
 	namespace resource_name {
 		#define DEF_NAME(name) constexpr const char* name = #name
 		DEF_NAME(RWops);
