1560a61423a2a5018baac80113082ed1a4d2936d
degarashi <slice013@gmail.com>
2018-01-09 17:14:18 +0900

    MakeUniform: 型変換を伴う値を指定した場合のコンパイルエラー修正

--- drawtoken/make_uniform.hpp
+++ drawtoken/make_uniform.hpp
@@ -9,13 +9,21 @@ namespace rev {
 		inline Token_SP MakeUniform(const bool* b, const int nvalue) {
 			return std::make_shared<Unif_Vec<bool,1>>(b, 1, nvalue);
 		}
+		template <class To, class From>
+		Token_SP _MakeUniform_To(const From* t, const int nvalue);
+		inline Token_SP MakeUniform(const float* f, const int nvalue) {
+			return std::make_shared<Unif_Vec<float,1>>(f, 1, nvalue);
+		}
 		template <class T, ENABLE_IF(std::is_floating_point<T>{})>
 		Token_SP MakeUniform(const T* t, const int nvalue) {
-			return std::make_shared<Unif_Vec<float,1>>(t, 1, nvalue);
+			return _MakeUniform_To<float>(t, nvalue);
+		}
+		inline Token_SP MakeUniform(const int32_t* t, const int nvalue) {
+			return std::make_shared<Unif_Vec<int32_t,1>>(t, 1, nvalue);
 		}
 		template <class T, ENABLE_IF(std::is_integral<T>{})>
 		Token_SP MakeUniform(const T* t, const int nvalue) {
-			return std::make_shared<Unif_Vec<int32_t,1>>(t, 1, nvalue);
+			return _MakeUniform_To<int32_t>(t, nvalue);
 		}
 		template <class V, ENABLE_IF(frea::is_vector<V>{})>
 		Token_SP MakeUniform(const V* v, const int nvalue) {
@@ -61,6 +69,13 @@ namespace rev {
 		template <class T>
 		Token_SP MakeUniform(const T& t) {
 			return MakeUniform(&t, 1);
+		}
+		template <class To, class From>
+		Token_SP _MakeUniform_To(const From* t, const int nvalue) {
+			std::vector<To> tmp(nvalue);
+			for(int i=0 ; i<nvalue ; i++)
+				tmp[i] = t[i];
+			return MakeUniform(tmp.data(), nvalue);
 		}
 	}
 }
