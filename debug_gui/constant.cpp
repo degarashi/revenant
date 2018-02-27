c2127e5a28b0e3f95bc2756040ead2343d89ec20
degarashi <slice013@gmail.com>
2018-02-27 11:02:35 +0900

    DebugGUI: ToStringを定義

--- debug_gui/constant.cpp
+++ debug_gui/constant.cpp
@@ -11,6 +11,15 @@ namespace rev {
 		const char* DefaultDisplayFormat<int> = "%d";
 		template <>
 		const char* DefaultDisplayFormat<unsigned int> = "%u";
+		template <class T>
+		std::string _ToString(const T t, std::true_type) {
+			char buff[64];
+			std::snprintf(buff, 64, DefaultDisplayFormat<T>, t);
+			return buff;
 		}
+		template std::string _ToString<double>(const double t, std::true_type);
+		template std::string _ToString<float>(const float t, std::true_type);
+		template std::string _ToString<int>(const int t, std::true_type);
+		template std::string _ToString<unsigned int>(const unsigned int t, std::true_type);
 	}
 }
--- debug_gui/constant.hpp
+++ debug_gui/constant.hpp
@@ -8,5 +8,22 @@ namespace rev {
 		extern const float EntrySpacing;
 		template <class T>
 		const char* DefaultDisplayFormat;
+
+		using DefaultDisplayTypes = lubee::Types<
+			double,
+			float,
+			int,
+			unsigned int
+		>;
+		template <class T>
+		std::string _ToString(const T t, std::true_type);
+		template <class T>
+		std::string _ToString(const T t, std::false_type) {
+			return std::to_string(t);
+		}
+		template <class T>
+		std::string ToString(const T t) {
+			return _ToString(t, DefaultDisplayTypes::Has<T>{});
+		}
 	}
 }
