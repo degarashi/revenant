51bea2d1a24ee61f8429b9d7ed94a093a1f39a8f
degarashi <slice013@gmail.com>
2018-03-02 18:55:43 +0900

    DebugGUI: ResMgrの表示レイアウトを変更

---
+++ debug_gui/types/resmgr.cpp
@@ -0,0 +1,9 @@
+#include "resmgr.hpp"
+
+namespace rev {
+	namespace debug {
+		namespace inner {
+			SelectPos g_selectPos;
+		}
+	}
+}
--- debug_gui/types/resmgr.hpp
+++ debug_gui/types/resmgr.hpp
@@ -2,7 +2,9 @@
 #include "../print.hpp"
 #include "spine/resmgr.hpp"
 #include "../../imgui/imgui.h"
-#include "../tree.hpp"
+#include "../column.hpp"
+#include "../child.hpp"
+#include <unordered_map>
 
 namespace rev {
 	namespace debug {
@@ -14,30 +16,39 @@ namespace rev {
 					ss.str("");
 					ss.clear();
 				};
-				for(auto&& r : m) {
-					const IdPush id(r.get());
-					ss << "address: 0x" << std::hex << r.get();
-					if(const auto t = TreePush(ss.str().c_str())) {
-						cb(*r);
+				if(const auto c = ChildPush("ResMgr", {0, 0}, false, ImGuiWindowFlags_HorizontalScrollbar)) {
+					for(auto&& r : m) {
+						const IdPush id(r.get());
+						ss << "address: 0x" << std::hex << r.get();
+						cb(ss.str().c_str(), r);
+						clear();
 					}
-					clear();
 				}
 			}
-		}
-		namespace inner {
-			template <class T, class A>
-			void _Show(const spi::ResMgr<T,A>& m) {
-				inner::ResMgr_Iter(m, [](auto&& r){
-					Show("", r);
-				});
-			}
+			using SelectPos = std::unordered_map<ImGuiID, std::weak_ptr<void>>;
+			extern SelectPos g_selectPos;
+
 			template <class T, class A>
 			bool _Edit(spi::ResMgr<T,A>& m) {
-				bool ret = false;
-				inner::ResMgr_Iter(m, [&ret](auto&& r){
-					ret |= Edit("", r);
+				const auto c = ColumnPush(2);
+				const auto id = ImGui::GetID("ResMgr");
+				auto itr = g_selectPos.find(id);
+				if(itr == g_selectPos.end()) {
+					itr = g_selectPos.emplace(id, std::weak_ptr<void>()).first;
+				}
+				auto& cur = itr->second;
+				const auto cur_lk = cur.lock();
+				inner::ResMgr_Iter(m, [&cur, &cur_lk](const char* name, auto&& r){
+					if(ImGui::Selectable(name, cur_lk == r))
+						cur = r;
 				});
-				return ret;
+
+				ImGui::NextColumn();
+				if(const auto sp = cur.lock()) {
+					using R = typename std::decay_t<decltype(*m.begin())>::element_type;
+					return Edit("", *reinterpret_cast<R*>(sp.get()));
+				}
+				return false;
 			}
 		}
 	}
