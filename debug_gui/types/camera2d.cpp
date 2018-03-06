29b138ec6c99d4a945ca421c020a926abc2b36b2
degarashi <slice013@gmail.com>
2018-03-06 14:57:45 +0900

    DebugGUI: Pose2Dクラスを廃止

--- debug_gui/types/camera2d.cpp
+++ debug_gui/types/camera2d.cpp
@@ -1,23 +1,19 @@
-#include "pose2d.hpp"
 #include "../../camera2d.hpp"
 #include "../../imgui/imgui.h"
 #include "../entry_field.hpp"
 
 namespace rev {
-	namespace {
-		const char	*lb_pose = "Pose",
-					*lb_aspect = "Aspect",
-					*lb_accum = "Accum";
-	}
 	bool Camera2D::guiEditor(bool) {
 		if(auto field = debug::EntryField("Camera2D")) {
-			if(field.edit(lb_pose, _rflag.ref<Pose>())) {
+			ImGui::Columns(1);
+			if(debug::Edit("pose", _rflag.ref<Pose>())) {
 				refPose();
 			}
-			if(field.edit(lb_aspect, _rflag.ref<AspectRatio>())) {
+			ImGui::Columns(2);
+			if(field.edit("Aspect", _rflag.ref<AspectRatio>())) {
 				setAspectRatio(getAspectRatio());
 			}
-			field.show(lb_accum, uint64_t(getAccum()));
+			field.show("Accum", uint64_t(getAccum()));
 			return field.modified();
 		}
 		return false;
--- debug_gui/types/pose2d.cpp
+++ debug_gui/types/pose2d.cpp
@@ -1,91 +1,49 @@
-#include "pose2d.hpp"
 #include "beat/pose2d.hpp"
-#include "vector.hpp"
+#include "debug_gui/types/vector.hpp"
 #include "../../imgui/imgui.h"
 #include "angle.hpp"
-#include "../id.hpp"
 #include "../indent.hpp"
-#include "../child.hpp"
+#include "../entry_field.hpp"
 
 namespace rev {
 	namespace debug {
-		namespace {
-			const char	*lb_posewindow = "pose-window",
-						*lb_offset = "Offset",
-						*lb_scale = "Scale",
-						*lb_rotation = "Rotation";
-		}
-		// ----------------- Pose2DC -----------------
-		Pose2DC::Pose2DC(const beat::g2::Pose& p):
-			_p(p)
-		{}
-		void Pose2DC::show() const {
-			if(const auto child = ChildPush(lb_posewindow, {0, ImGui::GetTextLineHeightWithSpacing()*7}, true, ImGuiWindowFlags_NoScrollbar)) {
-				ImGui::TextUnformatted(lb_offset);
-				{
-					const IndentPush ind;
-					const IdPush id("#0");
-					VectorC<frea::Vec2>(_p.getOffset()).show();
-				}
-				ImGui::TextUnformatted(lb_scale);
-				{
-					const IndentPush ind;
-					const IdPush id("#1");
-					VectorC<frea::Vec2>(_p.getScaling()).show();
-				}
-				ImGui::TextUnformatted(lb_rotation);
-				{
-					const IndentPush ind;
-					const IdPush id("#2");
-					AngleC<frea::DegF>(_p.getRotation()).show();
-				}
-			}
-		}
-
-		// ----------------- Pose2D -----------------
-		Pose2D::Pose2D(beat::g2::Pose& p):
-			Pose2DC(p),
-			_p(p)
-		{}
-		bool Pose2D::edit() const {
-			bool ret = false;
-			if(const auto child = ChildPush(lb_posewindow, {0, ImGui::GetTextLineHeightWithSpacing()*5}, true, ImGuiWindowFlags_NoScrollbar)) {
-				{
-					ImGui::TextUnformatted(lb_offset);
-					ImGui::SameLine();
-					auto tmp = _p.getOffset();
-					if(FVector<frea::AVec2>(tmp).edit("#0")) {
-						_p.setOffset(tmp);
-						ret = true;
-					}
-				}
-				{
-					ImGui::TextUnformatted(lb_scale);
-					ImGui::SameLine();
-					auto tmp = _p.getScaling();
-					if(FVector<frea::AVec2>(tmp).edit("#1")) {
-						_p.setScaling(tmp);
-						ret = true;
-					}
-				}
-				{
-					ImGui::TextUnformatted(lb_rotation);
-					ImGui::SameLine();
-					auto tmp = _p.getRotation();
-					if(Angle<frea::RadF>(tmp).edit("#2")) {
-						_p.setRotation(tmp);
-						ret = true;
-					}
-				}
-			}
-			return ret;
-		}
 		namespace inner {
+			namespace {
+				const char	*lb_pose2d = "Pose2D",
+							*lb_offset = "Offset",
+							*lb_scale = "Scale",
+							*lb_rotation = "Rotation";
+			}
 			void _Show(const ::beat::g2::Pose& p) {
-				Pose2DC(p).show();
+				if(auto f = EntryField(lb_pose2d, 0, 3)) {
+					f.show(lb_offset, p.getOffset());
+					f.show(lb_scale, p.getScaling());
+					f.show(lb_rotation, p.getRotation());
+				}
 			}
 			bool _Edit(::beat::g2::Pose& p) {
-				return Pose2D(p).edit();
+				if(auto f = EntryField(lb_pose2d, 0, 3)) {
+					{
+						auto tmp = p.getOffset();
+						if(f.edit(lb_offset, tmp)) {
+							p.setOffset(tmp);
+						}
+					}
+					{
+						auto tmp = p.getScaling();
+						if(f.edit(lb_scale, tmp)) {
+							p.setScaling(tmp);
+						}
+					}
+					{
+						auto tmp = p.getRotation();
+						if(f.edit(lb_rotation, tmp)) {
+							p.setRotation(tmp);
+						}
+					}
+					return f.modified();
+				}
+				return false;
 			}
 		}
 	}
