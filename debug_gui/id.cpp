30339765f3e9aec072550dc8e582a88ada713f29
degarashi <slice013@gmail.com>
2018-03-12 11:12:25 +0900

    DebugGUI: IdPush: nullptrを指定すると何もしない

--- debug_gui/id.cpp
+++ debug_gui/id.cpp
@@ -3,17 +3,32 @@
 
 namespace rev {
 	namespace debug {
-		IdPush::IdPush(const int id) {
+		IdPush::IdPush(std::nullptr_t):
+			_valid(false)
+		{}
+		IdPush::IdPush(IdPush&& idp):
+			_valid(idp._valid)
+		{
+			idp._valid = false;
+		}
+		IdPush::IdPush(const int id):
+			_valid(true)
+		{
 			ImGui::PushID(id);
 		}
-		IdPush::IdPush(const char* id) {
+		IdPush::IdPush(const char* id):
+			_valid(true)
+		{
 			ImGui::PushID(id);
 		}
-		IdPush::IdPush(const void* id) {
+		IdPush::IdPush(const void* id):
+			_valid(true)
+		{
 			ImGui::PushID(id);
 		}
 		IdPush::~IdPush() {
-			ImGui::PopID();
+			if(_valid)
+				ImGui::PopID();
 		}
 	}
 }
--- debug_gui/id.hpp
+++ debug_gui/id.hpp
@@ -1,8 +1,12 @@
 #pragma once
+#include <cstddef>
 
 namespace rev {
 	namespace debug {
 		struct IdPush {
+			bool	_valid;
+			IdPush(IdPush&& idp);
+			IdPush(std::nullptr_t);
 			IdPush(const int id);
 			IdPush(const char* id);
 			IdPush(const void* id);
