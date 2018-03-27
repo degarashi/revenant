0971829274a7683efc23fff63e3fe99156e20085
degarashi <slice013@gmail.com>
2018-03-27 17:42:05 +0900

    コメント付加など

--- drawtoken/buffer.cpp
+++ drawtoken/buffer.cpp
@@ -1,14 +1,11 @@
 #include "buffer.hpp"
 
-namespace rev {
-	namespace draw {
-		// --------------------------- draw::Buffer ---------------------------
-		Buffer::Buffer(const GLBufferCore& core, const HBuff& hBuff):
-			GLBufferCore(core),
-			_hBuff(hBuff)
-		{}
-		void Buffer::exec() {
-			use_begin();
-		}
+namespace rev::draw {
+	Buffer::Buffer(const GLBufferCore& core, const HBuff& hBuff):
+		GLBufferCore(core),
+		_hBuff(hBuff)
+	{}
+	void Buffer::exec() {
+		use_begin();
 	}
 }
--- drawtoken/buffer.hpp
+++ drawtoken/buffer.hpp
@@ -2,14 +2,17 @@
 #include "../gl_buffer.hpp"
 #include "../handle/opengl.hpp"
 
-namespace rev {
-	namespace draw {
-		class Buffer : public GLBufferCore, public TokenT<Buffer> {
-			private:
-				HBuff	_hBuff;
-			public:
-				Buffer(const GLBufferCore& core, const HBuff& hBuff);
-				void exec() override;
-		};
-	}
+namespace rev::draw {
+	class Buffer :
+		public GLBufferCore,
+		public TokenT<Buffer>
+	{
+		private:
+			// バッファの参照カウントを維持してるだけなのでこれ自体にはアクセスしない
+			// GLBufferCoreの情報だけ使う
+			HBuff	_hBuff;
+		public:
+			Buffer(const GLBufferCore& core, const HBuff& hBuff);
+			void exec() override;
+	};
 }
