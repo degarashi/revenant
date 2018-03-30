bcf0fb3979e473445d2cab52b92c6ae8233a2ae8
degarashi <slice013@gmail.com>
2018-03-30 14:06:02 +0900

    draw::MakeUniform(): 正方ではない行列が入力された場合、正方行列に内部変換

--- drawtoken/matrix.hpp
+++ drawtoken/matrix.hpp
@@ -15,10 +15,36 @@ namespace rev::draw {
 			mat_v		_data;
 
 		public:
-			template <class Itr>
+			// 正方行列
+			template <
+				class Itr,
+				class M = std::decay_t<decltype(*std::declval<Itr>())>,
+				ENABLE_IF(
+					M::dim_m == M::dim_n
+				)
+			>
 			Unif_MatA(const Itr itr, const Itr itrE):
 				_data(itr, itrE)
 			{}
+			// 正方行列ではない物については変換
+			template <
+				class Itr,
+				class M = std::decay_t<decltype(*std::declval<Itr>())>,
+				std::size_t MDim = lubee::Arithmetic<M::dim_m, M::dim_n>::great,
+				ENABLE_IF(
+					M::dim_m != M::dim_n
+				)
+			>
+			Unif_MatA(Itr itr, const Itr itrE):
+				_data(itrE - itr)
+			{
+				auto* dst = _data.data();
+				while(itr != itrE) {
+					*dst++ = itr->template convertI<Dim,Dim>(1);
+					++itr;
+				}
+				D_Assert0(dst == _data.data()+_data.size());
+			}
 			template <class M>
 			Unif_MatA(const std::vector<M>& src):
 				Unif_MatA(src.cbegin(), src.cend())
@@ -42,10 +68,28 @@ namespace rev::draw {
 			mat_t		_data;
 
 		public:
-			template <class M>
+			// 正方行列
+			template <
+				class M,
+				ENABLE_IF(
+					M::dim_m == M::dim_n
+				)
+			>
 			Unif_Mat(const M& m):
 				_data(m)
 			{}
+			// 正方行列ではない物については変換
+			template <
+				class M,
+				std::size_t MDim = lubee::Arithmetic<M::dim_m, M::dim_n>::great,
+				ENABLE_IF(
+					M::dim_m != M::dim_n
+				)
+			>
+			Unif_Mat(const M& m):
+				_data(m.template convertI<Dim,Dim>(1))
+			{}
+
 			void exec() override {
 				Unif_Mat_Exec(Dim-2, base_t::idUnif, &_data.m[0][0], 1, true);
 			}
