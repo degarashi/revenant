7d33521c9c18f5666c1feff1f7a813e83704b29f
degarashi <slice013@gmail.com>
2018-04-04 12:39:44 +0900

    DebugGUI: SceneMgrの描画をListViewを使った物に変更

--- debug_gui/types/scene_mgr.cpp
+++ debug_gui/types/scene_mgr.cpp
@@ -6,36 +6,13 @@
 #include "../state_storage.hpp"
 #include "../print.hpp"
 #include "../../imgui/imgui.h"
+#include "../listview.hpp"
 
 namespace rev {
-	bool SceneMgr::property(bool) {
+	bool SceneMgr::property(const bool) {
 		if(const auto c = debug::ChildPush("SceneMgr", {0,0})) {
-			const auto colmn = debug::ColumnPush(2);
-			ImGui::SetColumnWidth(0, 120);
-			const auto id = ImGui::GetID("");
-			using St = debug::StateStorage<std::weak_ptr<IDebugGui>>;
-			auto cur = St::Get<IScene>(id);
-			{
-				const auto idp = debug::IdPush("Left");
-				ImGui::TextUnformatted("SceneStack");
-				ImGui::Separator();
-				int idx = 0;
-				for(const auto& s : _scene) {
-					const auto str = lubee::log::MakeMessage("[%d] %s", idx, s->getDebugName());
-					if(ImGui::Selectable(str.c_str(), cur == s)) {
-						cur = s;
-						St::Set(id, cur);
-					}
-					++idx;
-				}
-			}
-			ImGui::NextColumn();
-			{
-				const auto idp = debug::IdPush("Right");
-				if(cur) {
-					debug::Edit("", cur);
-				}
-			}
+			debug::ListView(_scene.cbegin(), _scene.cend(),
+					debug::ListViewFlag::NarrowColumn | debug::ListViewFlag::Filter);
 		}
 		return false;
 	}
