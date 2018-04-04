52b05f10c87b88a99bc0cf03eae10926ab649485
degarashi <slice013@gmail.com>
2018-04-04 14:02:09 +0900

    handle/common: const版weak_ptrの宣言

--- handle/common.hpp
+++ handle/common.hpp
@@ -4,4 +4,5 @@
 #define DEF_HANDLE(typ, name) \
 	using H##name = std::shared_ptr<typ>; \
 	using H##name##C = std::shared_ptr<std::add_const_t<typ>>; \
-	using W##name = std::weak_ptr<typ>;
+	using W##name = std::weak_ptr<typ>; \
+	using W##name##C = std::weak_ptr<std::add_const_t<typ>>;
