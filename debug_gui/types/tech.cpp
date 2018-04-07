760e9ded18aeecba2133aeed5c66b5c50897c4f2
degarashi <slice013@gmail.com>
2018-04-07 12:51:29 +0900

    DebugGUI: Tech: Headerを利用して書き直し

--- debug_gui/types/tech.cpp
+++ debug_gui/types/tech.cpp
@@ -2,6 +2,7 @@
 #include "../entry_field.hpp"
 #include "../../gl_state.hpp"
 #include "../indent.hpp"
+#include "../header.hpp"
 
 namespace rev {
 	bool Tech::property(const bool edit) {
@@ -10,25 +11,32 @@ namespace rev {
 		f.show("Name", getName());
 		{
 			const auto _ = debug::ColumnPush(1);
-			if(ImGui::CollapsingHeader("DefaultParameter")) {
-				const auto _ = debug::IndentPush();
+			if(const auto _ = debug::Header("DefaultParameter", getDefaultValue().getIdEntry().empty())) {
 				auto& val = const_cast<UniformEnt&>(getDefaultValue());
 				mod |= val.property(edit);
 			}
-			if(const auto& p = getProgram()) {
-				if(ImGui::CollapsingHeader("NoDefaultParameter")) {
-					const auto _ = debug::IndentPush();
-					const auto& nodef = getNoDefaultValue();
-					for(auto id : nodef) {
-						const auto* name = p->getUniformName(id);
-						D_Assert0(name);
-						ImGui::TextUnformatted(name);
-					}
+
+			if(const auto _ = debug::Header("NoDefaultParameter", getNoDefaultValue().empty())) {
+				const auto& p = getProgram();
+				const auto& nodef = getNoDefaultValue();
+				for(auto id : nodef) {
+					const auto* name = p->getUniformName(id);
+					D_Assert0(name);
+					ImGui::TextUnformatted(name);
 				}
 			}
-			if(ImGui::CollapsingHeader("Enable / Disable")) {
-				const auto _ = debug::IndentPush();
-				int idx = 0;
+			std::size_t nBool=0,
+						nValue=0;
+			std::for_each(_setting.cbegin(), _setting.cend(),
+				[&nBool, &nValue](const auto& s){
+					if(s->getType() == GLState::Type::Value)
+						++nValue;
+					else
+						++nBool;
+				}
+			);
+			if(const auto _ = debug::Header("Enable / Disable", nBool==0)) {
+				std::size_t idx = 0;
 				for(auto& a : _setting) {
 					if(a->getType() == GLState::Type::Boolean) {
 						const auto __ = debug::IdPush(idx++);
@@ -36,9 +44,8 @@ namespace rev {
 					}
 				}
 			}
-			if(ImGui::CollapsingHeader("State")) {
-				const auto _ = debug::IndentPush();
-				int idx = 0;
+			if(const auto _ = debug::Header("State", nValue==0)) {
+				std::size_t idx = 0;
 				for(auto& a : _setting) {
 					if(a->getType() == GLState::Type::Value) {
 						const auto __ = debug::IdPush(idx++);
@@ -46,8 +53,7 @@ namespace rev {
 					}
 				}
 			}
-			if(ImGui::CollapsingHeader("VSemantics")) {
-				const auto _ = debug::IndentPush();
+			if(const auto _ = debug::Header("VSemantics", _vattr.empty())) {
 				{
 					const auto __ = debug::ColumnPush(2);
 					ImGui::TextUnformatted("AttributeId");
@@ -64,7 +70,7 @@ namespace rev {
 				}
 				ImGui::Separator();
 			}
-			if(ImGui::CollapsingHeader("Program")) {
+			if(const auto _ = debug::Header("Program", !getProgram())) {
 				debug::Show("Program", getProgram());
 			}
 		}
