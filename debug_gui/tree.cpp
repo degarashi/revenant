8cecc2542fe13c1416d0f958feaaddae85a9f542
degarashi <slice013@gmail.com>
2018-04-07 10:04:14 +0900

    DebugGUI: TreePush: フラグにNoTreePushOnOpenが指定された場合に落ちるのを修正

--- debug_gui/tree.cpp
+++ debug_gui/tree.cpp
@@ -5,6 +5,8 @@ namespace rev {
 	namespace debug {
 		TreePush::TreePush(const char* label, const ImGuiTreeNodeFlags flag) {
 			_valid = ImGui::TreeNodeEx(label, flag);
+			if(flag & ImGuiTreeNodeFlags_NoTreePushOnOpen)
+				_valid = false;
 		}
 		TreePush::TreePush(TreePush&& t):
 			_valid(t._valid)
