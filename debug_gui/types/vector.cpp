76d19e8ec528a33f15671e3cae3a0beb5553f790
degarashi <slice013@gmail.com>
2018-04-13 14:10:29 +0900

    DebugGUI: print() 要素数1のベクトル対応

--- debug_gui/types/vector.cpp
+++ debug_gui/types/vector.cpp
@@ -1,13 +1,17 @@
 #include "vector.hpp"
 
 namespace rev::debug::inner {
+	using Vec1 = frea::Vec_t<float, 1, false>;
+	using IVec1 = frea::Vec_t<int32_t, 1, false>;
 	template <class V>
 	void _ShowVector(const V& v) {
 		VectorC<V>(v).show();
 	}
+	template void _ShowVector(const Vec1& v);
 	template void _ShowVector(const frea::Vec2& v);
 	template void _ShowVector(const frea::Vec3& v);
 	template void _ShowVector(const frea::Vec4& v);
+	template void _ShowVector(const IVec1& v);
 	template void _ShowVector(const frea::IVec2& v);
 	template void _ShowVector(const frea::IVec3& v);
 	template void _ShowVector(const frea::IVec4& v);
@@ -20,12 +24,14 @@ namespace rev::debug::inner {
 			return IVector<V>(v).edit("");
 		}
 	}
-	template bool _EditVector<frea::Vec2>(frea::Vec2& v);
-	template bool _EditVector<frea::Vec3>(frea::Vec3& v);
-	template bool _EditVector<frea::Vec4>(frea::Vec4& v);
-	template bool _EditVector<frea::IVec2>(frea::IVec2& v);
-	template bool _EditVector<frea::IVec3>(frea::IVec3& v);
-	template bool _EditVector<frea::IVec4>(frea::IVec4& v);
+	template bool _EditVector(Vec1& v);
+	template bool _EditVector(frea::Vec2& v);
+	template bool _EditVector(frea::Vec3& v);
+	template bool _EditVector(frea::Vec4& v);
+	template bool _EditVector(IVec1& v);
+	template bool _EditVector(frea::IVec2& v);
+	template bool _EditVector(frea::IVec3& v);
+	template bool _EditVector(frea::IVec4& v);
 
 	template <class V>
 	bool _Slider_2(V& v, const typename V::value_t& v_min, const typename V::value_t& v_max, std::true_type) {
@@ -39,10 +45,11 @@ namespace rev::debug::inner {
 	bool _Slider(V& v, const typename V::value_t& v_min, const typename V::value_t& v_max) {
 		return _Slider_2(v, v_min, v_max, std::is_floating_point<typename V::value_t>{});
 	}
-	template bool _Slider<frea::Vec2>(frea::Vec2& v, const float& v_min, const float& v_max);
-	template bool _Slider<frea::Vec3>(frea::Vec3& v, const float& v_min, const float& v_max);
-	template bool _Slider<frea::Vec4>(frea::Vec4& v, const float& v_min, const float& v_max);
-	template bool _Slider<frea::AVec2>(frea::AVec2& v, const float& v_min, const float& v_max);
-	template bool _Slider<frea::AVec3>(frea::AVec3& v, const float& v_min, const float& v_max);
-	template bool _Slider<frea::AVec4>(frea::AVec4& v, const float& v_min, const float& v_max);
+	template bool _Slider(Vec1& v, const float& v_min, const float& v_max);
+	template bool _Slider(frea::Vec2& v, const float& v_min, const float& v_max);
+	template bool _Slider(frea::Vec3& v, const float& v_min, const float& v_max);
+	template bool _Slider(frea::Vec4& v, const float& v_min, const float& v_max);
+	template bool _Slider(frea::IVec2& v, const int32_t& v_min, const int32_t& v_max);
+	template bool _Slider(frea::IVec3& v, const int32_t& v_min, const int32_t& v_max);
+	template bool _Slider(frea::IVec4& v, const int32_t& v_min, const int32_t& v_max);
 }
