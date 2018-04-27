a5ef0a929f730f950091285a9fb8e3ba116546e0
degarashi <slice013@gmail.com>
2018-04-27 12:25:50 +0900

    ログ出力に時刻を記載

--- rev_output.cpp
+++ rev_output.cpp
@@ -1,5 +1,6 @@
 #include "rev_output.hpp"
 #include "sdl_tls.hpp"
+#include "rev_time.hpp"
 #include <boost/format.hpp>
 
 namespace rev {
@@ -7,12 +8,13 @@ namespace rev {
 		void RevOutput::print(const lubee::log::Type::e type, const std::string& s) {
 			std::string str;
 			// スレッド名を出力
-			boost::format msg("[%1%]:\n%2%");
+			boost::format msg("[%1%]: Time=%2%(ms)\n%3%");
+			const auto ms = float(std::chrono::duration_cast<Milliseconds>(GetCurrentTime()).count()) / 1000.f;
 			if(tls_threadName.initialized())
-				str = (msg % *tls_threadName % s).str();
+				str = (msg % *tls_threadName % ms % s).str();
 			else {
 				const auto thId = SDL_GetThreadID(nullptr);
-				str = (msg % thId % s).str();
+				str = (msg % thId % ms % s).str();
 			}
 			// スレッド間で出力が混ざらないようにロックをかける
 			const UniLock lk(_mutex);
