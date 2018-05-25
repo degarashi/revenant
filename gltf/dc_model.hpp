0c7a5dda4d3c5f3c5abb12af56ae3063744abea7
degarashi <slice013@gmail.com>
2018-05-25 11:43:41 +0900

    gltf::GLTFModelが使用するQM_FixedSequenceクラスをQM_Fixedに置き換え

--- gltf/dc_model.hpp
+++ gltf/dc_model.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include "../dc/model_if.hpp"
-#include "../dc/qm_fixedseq.hpp"
+#include "../dc/qm_fixed.hpp"
 #include "gltf/dc_common.hpp"
 
 namespace rev::gltf {
@@ -11,7 +11,7 @@ namespace rev::gltf {
 			MeshV		_mesh,
 						_skinmesh;
 			HTf			_tf;
-			mutable dc::QM_FixedSequence _qm;
+			mutable dc::QM_Fixed _qm;
 		public:
 			static HMdl FromScene(const Scene& s);
 			GLTFModel(const MeshV& mesh, const MeshV& skinmesh, const HTf& tf);
