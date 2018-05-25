57c91bdb5c0fe92b685095f4d7761eaa5371da40
degarashi <slice013@gmail.com>
2018-05-25 11:43:13 +0900

    参照元のノード構造 & Matrixの問い合わせ順が変化しないと仮定するキャッシュ機構(QM_Fixed)を定義

---
+++ dc/qm_fixed.cpp
@@ -0,0 +1,105 @@
+#include "qm_fixed.hpp"
+#include "lubee/hash_combine.hpp"
+#include "jat_if.hpp"
+#include "dc/node.hpp"
+
+namespace rev::dc {
+	// ------------------- QM_Fixed::Ent -------------------
+	QM_Fixed::Ent::Ent(const TfNode* node, const bool local, const std::size_t sid):
+		node(node),
+		local(local),
+		serialId(sid)
+	{}
+	void QM_Fixed::Ent::refresh() const {
+		if(local)
+			result = node->getPose().getToWorld().convertI<4,4>(1);
+		else
+			result = node->getTransform();
+	}
+	bool QM_Fixed::Ent::operator == (const Ent& e) const noexcept {
+		return node == e.node &&
+				local == e.local;
+	}
+
+	// ------------------- QM_Fixed::CacheUse -------------------
+	QM_Fixed::CacheUse::CacheUse(const CachePtrV& cache):
+		_cache(cache)
+	{}
+	void QM_Fixed::CacheUse::resetCursor() const {
+		_cursor = 0;
+	}
+	const Mat4& QM_Fixed::CacheUse::_getMat4() const {
+		D_Assert0(_cursor < _cache.size());
+		return _cache[_cursor++]->result;
+	}
+	Mat4 QM_Fixed::CacheUse::getLocal(const JointId) const {
+		return _getMat4();
+	}
+	Mat4 QM_Fixed::CacheUse::getGlobal(const JointId) const {
+		return _getMat4();
+	}
+	Mat4 QM_Fixed::CacheUse::getLocal(const SName&) const {
+		return _getMat4();
+	}
+	Mat4 QM_Fixed::CacheUse::getGlobal(const SName&) const {
+		return _getMat4();
+	}
+	// ------------------- QM_Fixed::Hash -------------------
+	std::size_t QM_Fixed::Hash::operator()(const Ent& e) const noexcept {
+		return lubee::hash_combine_implicit(e.node, e.local);
+	}
+	// ------------------- QM_Fixed -------------------
+	QM_Fixed::QM_Fixed(const IJointQuery& q):
+		_cacheUse(_cachePtr),
+		_source(q),
+		_serialId(0),
+		_initialized(false)
+	{}
+	const IQueryMatrix& QM_Fixed::prepareInterface() {
+		if(_initialized) {
+			if(_cacheV.empty()) {
+				_cacheV.resize(_cache.size());
+				for(auto& c : _cache)
+					_cacheV[c.serialId] = c;
+				for(auto*& c : _cachePtr)
+					c = &_cacheV[c->serialId];
+				_cache.clear();
+			}
+			for(auto& p : _cacheV)
+				p.refresh();
+
+			_cacheUse.resetCursor();
+			return _cacheUse;
+		}
+		_initialized = true;
+		return *this;
+	}
+	void QM_Fixed::_register(const TfNode* node, const bool local) const {
+		Ent ent(node, local, ~0);
+		auto itr = _cache.find(ent);
+		if(itr == _cache.end()) {
+			itr = _cache.emplace(Ent(node, local, _serialId++)).first;
+		}
+		_cachePtr.emplace_back(&(*itr));
+	}
+	template <class Key>
+	Mat4 QM_Fixed::_getMat(const Key& key, const bool local) const {
+		const TfNode* node = _source.queryJoint(key);
+		_register(node, local);
+		if(local)
+			return node->getPose().getToWorld().convertI<4,4>(1);
+		return node->getTransform();
+	}
+	Mat4 QM_Fixed::getLocal(const JointId id) const {
+		return _getMat(id, true);
+	}
+	Mat4 QM_Fixed::getGlobal(const JointId id) const {
+		return _getMat(id, false);
+	}
+	Mat4 QM_Fixed::getLocal(const SName& name) const {
+		return _getMat(name, true);
+	}
+	Mat4 QM_Fixed::getGlobal(const SName& name) const {
+		return _getMat(name, false);
+	}
+}
---
+++ dc/qm_fixed.hpp
@@ -0,0 +1,67 @@
+#pragma once
+#include "qm_if.hpp"
+
+namespace rev::dc {
+	class TfNode;
+	struct IJointQuery;
+	// 参照元のノード構造、およびQueryMatrixの呼び出し順が変化しないと仮定するキャッシュ機構
+	class QM_Fixed : public IQueryMatrix {
+		private:
+			struct Ent {
+				const TfNode*	node;
+				bool			local;
+
+				std::size_t		serialId;
+				mutable Mat4	result;
+
+				Ent() = default;
+				Ent(const TfNode* node, bool local, std::size_t sid);
+				void refresh() const;
+				bool operator == (const Ent& e) const noexcept;
+			};
+			struct Hash {
+				std::size_t operator()(const Ent& e) const noexcept;
+			};
+
+			using Cache = std::unordered_set<Ent, Hash>;
+			using CacheV = std::vector<Ent>;
+			using CachePtrV = std::vector<const Ent*>;
+			mutable Cache		_cache;
+			mutable CacheV		_cacheV;
+			mutable CachePtrV	_cachePtr;
+
+			class CacheUse : public IQueryMatrix {
+				private:
+					const CachePtrV&		_cache;
+					mutable std::size_t		_cursor;
+
+					const Mat4& _getMat4() const;
+				public:
+					CacheUse(const CachePtrV& cache);
+					void resetCursor() const;
+
+					Mat4 getLocal(JointId id) const override;
+					Mat4 getGlobal(JointId id) const override;
+					Mat4 getLocal(const SName& name) const override;
+					Mat4 getGlobal(const SName& name) const override;
+			};
+			CacheUse			_cacheUse;
+			const IJointQuery&	_source;
+			mutable std::size_t	_serialId;
+			bool				_initialized;
+
+			void _register(const TfNode* node, bool local) const;
+			template <class Key>
+			Mat4 _getMat(const Key& key, bool local) const;
+
+		public:
+			QM_Fixed(const QM_Fixed&) = delete;
+			QM_Fixed(const IJointQuery& q);
+			const IQueryMatrix& prepareInterface();
+
+			Mat4 getLocal(JointId id) const override;
+			Mat4 getGlobal(JointId id) const override;
+			Mat4 getLocal(const SName& name) const override;
+			Mat4 getGlobal(const SName& name) const override;
+	};
+}
