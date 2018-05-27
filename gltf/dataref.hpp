50908824067e12354a00e9ae32b53bab5f780b3f
degarashi <slice013@gmail.com>
2018-05-27 14:07:14 +0900

    gltf::DataRef: タグ検索に用いる変数型をテンプレートで指定できるようにした

--- gltf/dataref.hpp
+++ gltf/dataref.hpp
@@ -5,7 +5,8 @@
 namespace rev::gltf {
 	template <
 		class T,
-		const T& (IDataQuery::*FP)(const Tag&) const
+		class Tag_t,
+		const T& (IDataQuery::*FP)(const Tag_t&) const
 	>
 	class DataRef {
 		private:
@@ -15,7 +16,7 @@ namespace rev::gltf {
 			DataRef(const JValue& v, const IDataQuery& q):
 				_data(&(q.*FP)(loader::StdString(v)))
 			{}
-			DataRef(const Tag& tag, const IDataQuery& q):
+			DataRef(const Tag_t& tag, const IDataQuery& q):
 				_data(&(q.*FP)(tag))
 			{}
 			static bool CanLoad(const JValue& v) {
@@ -35,7 +36,7 @@ namespace rev::gltf {
 			}
 	};
 	#define DEF_REF(z, ign, name) \
-		using BOOST_PP_CAT(DRef_, name) = DataRef<name, &IDataQuery::BOOST_PP_CAT(get, name)>;
+		using BOOST_PP_CAT(DRef_, name) = DataRef<name, Tag, &IDataQuery::BOOST_PP_CAT(get, name)>;
 	BOOST_PP_SEQ_FOR_EACH(DEF_REF, EMPTY, SEQ_RES)
 	#undef DEF_REF
 }
