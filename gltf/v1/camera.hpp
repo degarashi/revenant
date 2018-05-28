46ea2f592f057c0e6abfd3c16e2a7868852a8487
degarashi <slice013@gmail.com>
2018-05-28 17:24:15 +0900

    Camera: v2.0との共通部分を切り出し

--- gltf/v1/camera.hpp
+++ gltf/v1/camera.hpp
@@ -1,37 +1,13 @@
 #pragma once
 #include "gltf/v1/resource.hpp"
-#include "frea/angle.hpp"
-#include <boost/variant.hpp>
+#include "gltf/camera.hpp"
 
-namespace rev {
-	class Camera3D;
-	using HCam3 = std::shared_ptr<Camera3D>;
-}
 namespace rev::gltf::v1 {
-	struct Camera : Resource {
-		struct Perspective {
-			float		aspectRatio;
-			frea::RadF	yfov;
-			float		zfar,
-						znear;
-			Perspective(const JValue& v);
-		};
-		struct Orthographic {
-			float	xmag,
-					ymag,
-					zfar,
-					znear;
-			Orthographic(const JValue& v);
-		};
-		using Value = boost::variant<Perspective, Orthographic>;
-		Value	value;
-
+	struct Camera :
+		gltf::Camera,
+		Resource
+	{
 		Camera(const JValue& v);
 		Type getType() const noexcept override;
-		Value _init(const JValue& v);
-
-		// デフォルト姿勢のカメラを作成
-		// (Perspectiveのみの対応)
-		HCam3 makeCamera() const;
 	};
 }
