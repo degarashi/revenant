aadabc41f2f91103b6feb53d34bcfb1818249f65
degarashi <slice013@gmail.com>
2018-06-01 09:45:18 +0900

    gltf::DataPを定義

---
+++ gltf/data_pair.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <cstdint>
+
+namespace rev::gltf {
+	struct DataP {
+		uintptr_t	pointer;
+		std::size_t	length;
+
+		void* asPointer() noexcept {
+			return reinterpret_cast<void*>(pointer);
+		}
+		const void* asPointer() const noexcept {
+			return reinterpret_cast<const void*>(pointer);
+		}
+	};
+}
