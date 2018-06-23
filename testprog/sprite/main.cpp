4e0362c3491bfc8fc0c8f4f783de32d0c8e9e48b
degarashi <slice013@gmail.com>
2018-06-23 11:57:03 +0900

    不要な構造体、ファイルを削除(shared.hpp)

--- testprog/sprite/main.cpp
+++ testprog/sprite/main.cpp
@@ -1,11 +1,9 @@
-#include "shared.hpp"
 #include "param.hpp"
 #include "../test.hpp"
 #include "../../guithread.hpp"
 
 namespace rev {
 	namespace test {
-		SpinLock<UserShare>	g_shared;
 		struct GameloopTest : Random {};
 		TEST_F(GameloopTest, General) {
 			GUIThread loop(std::make_unique<Param>());
--- testprog/sprite/shared.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "../../spinlock.hpp"
-
-namespace rev::test {
-	struct UserShare {};
-	extern SpinLock<UserShare> g_shared;
-}
