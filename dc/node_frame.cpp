2a947ccd1136878f76cd994e638172aa367112a0
degarashi <slice013@gmail.com>
2018-07-05 17:15:06 +0900

    Node_FrameOutのリファクタリング

--- dc/node_frame.cpp
+++ dc/node_frame.cpp
@@ -7,41 +7,46 @@ namespace rev::dc {
 	std::size_t Node_FrameOut::numFrame() const {
 		return value->size() / getNUnit();
 	}
-	template <class Res>
-	Res Node_FrameOut::_calcValue(const std::size_t idx, const float t) const {
+	const float* Node_FrameOut::_calcValue(const std::size_t idx, const float t) const {
 		D_Assert0(lubee::IsInRange(t, 0.f, 1.f));
 		const auto NUnit = getNUnit();
 		auto& val = *value;
 		D_Assert0((idx+1)*NUnit < val.size());
 
-		const auto getElem = [vptr = val.data(), NUnit](const std::size_t n){
-			return *reinterpret_cast<const Res*>(vptr + n * NUnit);
-		};
 		if(t == 0.f)
-			return getElem(idx);
-		else
-			return frea::Lerp(getElem(idx), getElem(idx+1), t);
+			for(std::size_t i=0 ; i<NUnit ; i++)
+				tmp[i] = val[idx * NUnit + i];
+		else {
+			frea::Lerp(
+				tmp,
+				val.data() + idx*NUnit,
+				val.data() + (idx+1)*NUnit,
+				val.data() + (idx+1)*NUnit,
+				t
+			);
+		}
+		return tmp;
 	}
 
 	std::size_t Node_T_FrameOut::getNUnit() const noexcept {
 		return 3;
 	}
 	void Node_T_FrameOut::output(TfNode& dst, const std::size_t idx, const float t) const {
-		const auto res = _calcValue<frea::Vec3>(idx, t);
-		dst.refPose().setOffset(res);
+		auto* res = reinterpret_cast<const frea::Vec3*>(_calcValue(idx, t));
+		dst.refPose().setOffset(*res);
 	}
 	std::size_t Node_R_FrameOut::getNUnit() const noexcept {
 		return 4;
 	}
 	void Node_R_FrameOut::output(TfNode& dst, const std::size_t idx, const float t) const {
-		const auto res = _calcValue<frea::Quat>(idx, t);
-		dst.refPose().setRotation(res);
+		auto* res = reinterpret_cast<const frea::Quat*>(_calcValue(idx, t));
+		dst.refPose().setRotation(*res);
 	}
 	std::size_t Node_S_FrameOut::getNUnit() const noexcept {
 		return 3;
 	}
 	void Node_S_FrameOut::output(TfNode& dst, const std::size_t idx, const float t) const {
-		const auto res = _calcValue<frea::Vec3>(idx, t);
-		dst.refPose().setScaling(res);
+		auto* res = reinterpret_cast<const frea::Vec3*>(_calcValue(idx, t));
+		dst.refPose().setScaling(*res);
 	}
 }
--- dc/node_frame.hpp
+++ dc/node_frame.hpp
@@ -3,15 +3,16 @@
 #include <memory>
 
 namespace rev::dc {
-	using FVec = std::shared_ptr<std::vector<float>>;
-
 	struct Node_FrameOut : IFrameOut {
-		FVec		value;
+		using FVec_S = std::shared_ptr<std::vector<float>>;
+		FVec_S		value;
+
+		constexpr static std::size_t N_Tmp = 32;
+		mutable float	tmp[N_Tmp];
 
 		virtual std::size_t getNUnit() const noexcept = 0;
 		std::size_t numFrame() const override;
-		template <class Res>
-		Res _calcValue(std::size_t idx, float t) const;
+		const float* _calcValue(std::size_t idx, float t) const;
 	};
 	struct Node_T_FrameOut : Node_FrameOut {
 		std::size_t getNUnit() const noexcept override;
