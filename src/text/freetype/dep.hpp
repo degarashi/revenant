e186c002466f8d258b59bcb3f310536061b92998
degarashi <slice013@gmail.com>
2019-03-30 20:59:45 +0900

    [update]Font_FTDepクラスの不要なoperator = 宣言を削除

--- src/text/freetype/dep.hpp
+++ src/text/freetype/dep.hpp
@@ -68,7 +68,6 @@ namespace rev {
 		public:
 			Font_FTDep(Font_FTDep&&) = default;
 			Font_FTDep(const FontName &name, FontId fid);
-			Font_FTDep& operator = (Font_FTDep&& dep) = default;
 
 			//! 結果的にFontIdが同じになるパラメータの値を統一
 			/*! (依存クラスによってはサイズが縦しか指定できなかったりする為) */
